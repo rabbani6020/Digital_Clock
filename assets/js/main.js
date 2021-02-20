142a3f14284845ca8c2447e95d48c4992af7c940
rabbani6020 <rabbani.cse.69@gmail.com>
2021-02-21 02:13:37 +0600

    project done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,36 @@
 const hour = document.querySelector(".hour");
 const minute = document.querySelector(".minute");
 const second = document.querySelector(".second");
+const timeFormat = document.querySelector(".time-format");
 
-let presentTime = () => {
-   const date = new Date();
-   const hr = date.getHours();
-   const min = date.getMinutes();
-   const sec = date.getSeconds();
+const presentTime = () => {
+   let date = new Date();
+   let hr = date.getHours();
+   let min = date.getMinutes();
+   let sec = date.getSeconds();
+   let format = "AM";
 
+   if (hr === 0) {
+      hr = 12;
+   }
+   //    hr = hr === 0 ? (hr = 12) : hr;
+   if (hr > 12) {
+      hr -= 12;
+      format = "PM";
+   }
+
+   // delemeter less than 10 then add 0
+   hr = hr < 10 ? "0" + hr : hr;
+   min = min < 10 ? "0" + min : min;
+   sec = sec < 10 ? "0" + sec : sec;
+
+   // data pass in the html
    hour.innerText = hr;
    minute.innerText = min;
    second.innerText = sec;
+   timeFormat.innerText = format;
+
+   setInterval(presentTime, 1000);
 };
 
-setInterval(presentTime, 1000)
+presentTime();

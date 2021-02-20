7202755df5d73caceb60ac77d2e44560140f7077
rabbani6020 <rabbani.cse.69@gmail.com>
2021-02-20 22:00:46 +0600

    UI Complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,15 @@ const hour = document.querySelector(".hour");
 const minute = document.querySelector(".minute");
 const second = document.querySelector(".second");
 
-const date = new Date();
+let presentTime = () => {
+   const date = new Date();
+   const hr = date.getHours();
+   const min = date.getMinutes();
+   const sec = date.getSeconds();
 
-const hr = date.getHours();
-const min = date.getMinutes();
-const sec = date.getSeconds();
+   hour.innerText = hr;
+   minute.innerText = min;
+   second.innerText = sec;
+};
 
-hour.innerText = hr;
-minute.innerText = min;
-second.innerText = sec;
+setInterval(presentTime, 1000)
